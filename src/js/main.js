0300b0ce0c362916826896f59faffdc75cea3f5e
Francheska Contreras <159475764+francheskajcg@users.noreply.github.com>
2026-06-12 00:00:50 +0200

    cambios aceptados en quienes somos

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,42 @@ if (navbar) {
     window.addEventListener('scroll', toggleNavbarState, { passive: true });
     window.addEventListener('resize', toggleNavbarState);
 }
+
+const navLinks = Array.from(document.querySelectorAll('.nav-menu a'));
+
+if (navLinks.length) {
+    const path = window.location.pathname.split('/').pop() || 'index.html';
+    const hash = window.location.hash;
+
+    const clearActive = () => {
+        navLinks.forEach((link) => link.classList.remove('is-active'));
+    };
+
+    const setActiveLink = () => {
+        const currentHash = window.location.hash;
+        let activeLink = null;
+
+        if (path === 'more-info.html') {
+            activeLink = navLinks.find((link) => link.getAttribute('href') === './more-info.html');
+        } else if (path === 'index.html' || path === '') {
+            if (currentHash) {
+                activeLink = navLinks.find((link) => link.getAttribute('href') === `./index.html${currentHash}`);
+            }
+
+            if (!activeLink) {
+                activeLink = navLinks.find((link) => link.getAttribute('href') === './index.html#inicio');
+            }
+        }
+
+        clearActive();
+        if (activeLink) {
+            activeLink.classList.add('is-active');
+        }
+    };
+
+    setActiveLink();
+
+    if (path === 'index.html' || path === '') {
+        window.addEventListener('hashchange', setActiveLink);
+    }
+}
